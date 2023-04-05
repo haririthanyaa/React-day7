29ecedcdc5bf00410889a4763ac607112cae70df
haririthanyaa <128139401+haririthanyaa@users.noreply.github.com>
2023-04-05 09:42:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import FetchNews from './Component/Fetch';
+function App() {
+  return (
+    <div className="App">
+    <FetchNews/>  
+    </div>
+  );
+}
+
+export default App;
